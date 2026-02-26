7a0fb88f8e16ca734c009ac8248dd5bbee1f0bb2
AntonioContrerasO-Avahi <antonio.contreras@avahi.ai>
2026-02-26 12:06:30 -0800

    feat: refactor code to divide into tasks

--- src/task-1-tests.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-BASE_URL = "http://127.0.0.1:8000/api/v1/"
-
-names = ["Antonio", "Maria"]
-
-for name in names:
-    response = requests.get(BASE_URL, params={"name": name}, headers={"accept": "application/json"})
-    print(f"[{response.status_code}] {name} -> {response.json()}")
